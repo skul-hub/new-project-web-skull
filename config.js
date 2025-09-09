ee8e7564db408cf5b06de6c659e6a40105d02380
skul-hub <yoelafrido42@gmail.com>
2025-09-09 10:41:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 // Supabase Config
 // Ganti dengan URL Supabase Anda
-const supabaseUrl = 'https://grcgachetkbbxgdewcty.supabase.co'; 
+const supabaseUrl = 'https://dvnfqoektrgarlplkkaj.supabase.co'; 
 // Ganti dengan Anon Key Anda
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdyY2dhY2hldGtiYnhnZGV3Y3R5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcwMzM1MzcsImV4cCI6MjA3MjYwOTUzN30.AxFhUICmyR3IP16Fox9a_7rqu7JCsAQbmB59CR0M-CA'; 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2bmZxb2VrdHJnYXJscGxra2FqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxMDI2MzksImV4cCI6MjA3MjY3ODYzOX0.c_T4297HqKGo3KZYvWM6qXkiw4saE548Cp-UH7qZ7Ts'; 
 
 // Inisialisasi Supabase client
 window.supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
